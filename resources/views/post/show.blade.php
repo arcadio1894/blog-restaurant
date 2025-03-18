d80125c7a1107ab1ff4c0de1fe61cbfca6f44464
arcadio1894 <joryes1894@gmail.com>
2025-03-18 11:31:07 -0500

    Cambios en el post show 6

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -107,5 +107,6 @@
 @endsection
 
 @section('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="{{ asset('js/blog/show.js') }}?v={{ time() }}"></script>
 @endsection
