e53fd80842debf6308f661f8df5f4e0dcabed0c1
arcadio1894 <joryes1894@gmail.com>
2025-03-18 11:40:31 -0500

    Cambios en el post show 9

--- public/js/blog/show.js
+++ public/js/blog/show.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // 1. Elimina atributos style innecesarios en cualquier etiqueta dentro de .text_description_show
+    // 1. Elimina atributos 'style' innecesarios de cualquier etiqueta dentro de .text_description_show
     $('.text_description_show *').removeAttr('style');
 
     // 2. Elimina <p> que solo contienen <br>, &nbsp; o están vacíos
@@ -18,14 +18,13 @@ $(document).ready(function() {
         }
     });
 
-    // 4. Elimina espacios normales y &nbsp; al inicio de cualquier etiqueta
-    $('.text_description_show *').each(function() {
-        let content = $(this).html();
-        if (content) {
-            $(this).html(content.replace(/^(\s|&nbsp;)+/, '')); // Elimina espacios y &nbsp; al inicio
+    // 4. Busca *cualquier* nodo de texto dentro de .text_description_show y elimina espacios al inicio
+    $('.text_description_show *').contents().each(function() {
+        if (this.nodeType === 3) { // Nodo de texto
+            this.nodeValue = this.nodeValue.replace(/^\s+|\u00A0+/g, ''); // Elimina espacios y &nbsp; al inicio
         }
     });
 
-    // 5. Si hay varios <p> vacíos seguidos, los elimina
+    // 5. Si hay <p> vacíos después de limpiar, los elimina
     $('.text_description_show p:empty').remove();
 });
