6e19a6629e05c6f313f45de79df249b3ccfa71ef
arcadio1894 <joryes1894@gmail.com>
2025-03-18 11:33:27 -0500

    Cambios en el post show 7

--- public/js/blog/show.js
+++ public/js/blog/show.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
-    // Elimina style="font-size: 1rem;" en cualquier etiqueta
+    // 1. Elimina el style="font-size: 1rem;" en cualquier etiqueta
     $('.text_description_show *[style*="font-size: 1rem;"]').removeAttr('style');
 
-    // Elimina <p><br></p>
+    // 2. Elimina <p> que solo contienen <br> o están vacíos
     $('.text_description_show p').each(function() {
-        if ($(this).html().trim() === '<br>') {
+        let content = $(this).html().trim();
+        if (content === '' || content === '<br>' || content === '&nbsp;') {
             $(this).remove();
         }
     });
 
-    // Elimina espacios normales y &nbsp; al inicio del contenido de cualquier etiqueta dentro de .text_description_show
+    // 3. Elimina <span> vacíos o con solo espacios (&nbsp;)
+    $('.text_description_show span').each(function() {
+        let content = $(this).html().trim();
+        if (content === '' || content === '&nbsp;') {
+            $(this).remove();
+        }
+    });
+
+    // 4. Elimina espacios normales y &nbsp; al inicio de cualquier etiqueta dentro de .text_description_show
     $('.text_description_show *').each(function() {
         let content = $(this).html();
         $(this).html(content.replace(/^(\s|&nbsp;)+/, '')); // Elimina espacios y &nbsp; al inicio
